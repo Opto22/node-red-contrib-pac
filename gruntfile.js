a3b6bef943d96c003e5d561c20a2ec284ddc8746
Jim Turner <jturner@opto22.com>
2018-04-20 15:26:52 -0700

    Grunt tasks for running tests against either SNAP or Groov systems

--- gruntfile.js
+++ gruntfile.js
@@ -21,6 +21,16 @@ module.exports = function(grunt) {
         src: 'src/test/external/settings.json',
         dest:'build/test/external/settings.json'
       },
+      testSettingsSnap: {
+        nonull: true,
+        src: 'src/test/external/settings.snapPac.json',
+        dest:'src/test/external/settings.json'
+      },
+      testSettingsGroov: {
+        nonull: true,
+        src: 'src/test/external/settings.groovPac.json',
+        dest:'src/test/external/settings.json'
+      },      
       build: {
         files: [
           {src: 'src/*.html',      dest: 'build/',       flatten: true, expand:  true},
@@ -54,6 +64,17 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks("grunt-simple-mocha");
   grunt.registerTask("default", ["clean:build", "copy:build", "ts"]);
   grunt.registerTask("test-internal", 'comment', ['simplemocha:internal']);
-  grunt.registerTask("test-all", 'comment', ['default', 'copy:testSettings', 'simplemocha:internal', 'simplemocha:external']);
+
+  /* Standard test task. Uses "src/test/external/settings.json". */
+  grunt.registerTask("test", 'comment', ['default', 'copy:testSettings', 'simplemocha:internal', 'simplemocha:external']);
+
+  /* Extra test tasks to easily test both Groov PAC and SNAP PAC.
+     For Groov PAC, uses "src/test/external/settings.groovPac.json". 
+     For SNAP PAC,  uses "src/test/external/settings.snapPac.json". 
+     IMPORTANT: THIS WILL COPY OVER THE EXISTING "src/test/external/settings.json" file!!
+  */
+  grunt.registerTask("test-snap",  'comment', ['copy:testSettingsSnap',  'test']);
+  grunt.registerTask("test-groov", 'comment', ['copy:testSettingsGroov', 'test']);
+
   grunt.registerTask("package", 'comment', ['clean:package', 'default', 'copy:package', 'npm-command:pack']);
 };
